2d32547cd5133421a82eeee6ae66c122b0fd8fac
SAMKIEL <samuelezekiel488@gmail.com>
2026-03-16 03:36:28 +0100

    chore: remove file

--- server/routes/analytics.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.get("/stats", (req, res) => {
-  res.json({ message: "Analytics stats route placeholder" });
-});
-
-module.exports = router;
